a0f711e30c44504caa81c04aab5d040270fe3d65
Babygrim <bstetskiv@gmail.com>
2023-03-05 22:06:42 +0200

    added fun to parse from silpo

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', get_price, name='get_price'),
+    path('', silpo, name='get_price'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+
 import requests
 from bs4 import BeautifulSoup
 from .forms import ProductForm
@@ -15,35 +10,45 @@ def home(request):
 
 
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
-def get_price(request):
+
+def silpo(request):
     if request.method == 'POST':
         product_name = request.POST['product_name']
-
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        driver = webdriver.Chrome(options=options)
-        url = f"https://shop.silpo.ua/search/all?find={product_name}"
-        driver.get(url)
+        options = webdriver.ChromeOptions()
+        options.add_argument("--disable")
+        options.add_argument("--headless")
+        driver = webdriver.Chrome("D:\Проект\chromedriver.exe", options=options)
+        driver.get(f'https://shop.silpo.ua/search/all?find={product_name}')
         try:
-            element_present = EC.presence_of_element_located((By.CLASS_NAME, WebDriverWait(driver, 10).until(element_present)))
+            myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'span')))
+            print ("Page is ready!")
         except TimeoutException:
-            return render(request, 'main/price.html', {'price': 'Ціну не вдалося знайти'})
-        product_link = driver.find_element_by_class_name('.search-results-list .product-title a')
-        product_link.click()
+            print ("Loading took too much time!")
 
-        try:
-            element_present = EC.presence_of_element_located((By.CLASS_NAME, WebDriverWait(driver, 10).until(element_present)))
-        except TimeoutException:
-            return render(request, 'main/price.html', {'price': 'Ціну не вдалося знайти'})
-
-        price_element = driver.find_element_by_class_name('product-current')
-        price = price_element.text.strip()
-
+        title = driver.find_elements(By.CLASS_NAME, "product-title")
+        response_title = []
+        for tit in title:
+            response_title.append(tit.text)
+        
+        price = driver.find_elements(By.CLASS_NAME, "current-integer")
+        response_price = []
+        for pri in price:
+            response_price.append(pri.text)
+        
+        mylist = zip(response_title, response_price)
+        context = {
+                    'mylist': mylist,
+        }
+        # Close the browser and return the price
         driver.quit()
-        return render(request, 'main/price.html', {'price': price})
+        return render(request, 'main/price.html', context)
     else:
         return render(request, 'main/get_price.html')
-
 
